7f3dab0bc0a952c2f84c6b84a9e48d3e567b6fd0
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-09-02 11:17:26 -0500

    🚧 base para calcular distancia entre coordenadas

--- aplicaciones/www/src/Aplicacion.vue
+++ aplicaciones/www/src/Aplicacion.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Hola from './componentes/Hola.vue';
+import { distanciaEntreCoordenadas } from './utilidades/ayudas';
 
 async function cargarDatos() {
   try {
@@ -11,6 +12,25 @@ async function cargarDatos() {
 }
 
 cargarDatos().catch(console.error);
+
+const puntos = [
+  [4.597813, -74.07569],
+  [4.601444, -74.073396],
+  [4.605297, -74.071588],
+  [4.61134, -74.069874],
+  [4.618599, -74.068069],
+  [4.62475, -74.066423],
+  [4.631393, -74.064257],
+  [4.6282521, -74.0670904],
+  [4.639489, -74.062897],
+];
+
+for (let i = 1; i < puntos.length; i++) {
+  const puntoA = puntos[i - 1];
+  const puntoB = puntos[i];
+
+  console.log(i, i + 2, `${distanciaEntreCoordenadas(puntoA[0], puntoA[1], puntoB[0], puntoB[1])}km`);
+}
 </script>
 
 <template>
---
+++ aplicaciones/www/src/utilidades/ayudas.ts
@@ -0,0 +1,25 @@
+export const gradosARadianes = (grados: number) => grados * (Math.PI / 180);
+
+/**
+ * Calcula distancia en kilómetros entre dos puntos/coordenadas.
+ *
+ * @param lat1 Latitud del punto 1.
+ * @param lon1 Longitud del punto 1.
+ * @param lat2 Latitud del punto 2.
+ * @param lon2 Longitud del punto 2.
+ * @returns Distancia en kilómetros entre los dos puntos.
+ */
+export const distanciaEntreCoordenadas = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+  // Radio del planeta en KM
+  const radio = 6371;
+  const dLat = gradosARadianes(lat2 - lat1);
+  const dLon = gradosARadianes(lon2 - lon1);
+  const centroLat = dLat / 2;
+  const centroLon = dLon / 2;
+  const a =
+    Math.sin(centroLat) * Math.sin(centroLat) +
+    Math.cos(gradosARadianes(lat1)) * Math.cos(gradosARadianes(lat2)) * Math.sin(centroLon) * Math.sin(centroLon);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return radio * c;
+};
