065f0478a2ec5003abeed3b549fa5bd74992e8c5
anattolia <anattolia@gmail.com>
2024-09-11 11:57:44 -0500

    💄 Mostrar todas las líneas

--- aplicaciones/www/src/componentes/VisualizacionIndices.vue
+++ aplicaciones/www/src/componentes/VisualizacionIndices.vue
@@ -22,12 +22,12 @@ let distanciaParcial: number = 0;
 let lineaHabitabilidad: string = `M 0 ${alturaContenedor}`;
 let lineaAmbiente: string = `M 0 ${alturaContenedor}`;
 let lineaInfraestructura: string = `M 0 ${alturaContenedor}`;
+let lineaMovilidad: string = `M 0 ${alturaContenedor}`;
+let lineaSeguridad: string = `M 0 ${alturaContenedor}`;
 
 onMounted(async () => {
   // Cargar datos
   const puntos = await fetch('/datos/puntos.json').then((res) => res.json());
-
-  const contenedorCalles: SVGElement = document.getElementById('contenedorCalles') as HTMLElement & SVGElement;
 
   // Habitabilidad
   const trazoHabitabilidad: SVGPathElement = document.getElementById('trazoHabitabilidad') as HTMLElement &
@@ -45,6 +45,14 @@ onMounted(async () => {
   const circulosInfraestructura: SVGElement = document.getElementById('circulosInfraestructura') as HTMLElement &
     SVGElement;
 
+  // Movilidad
+  const trazoMovilidad: SVGPathElement = document.getElementById('trazoMovilidad') as HTMLElement & SVGPathElement;
+  const circulosMovilidad: SVGElement = document.getElementById('circulosMovilidad') as HTMLElement & SVGElement;
+
+  // Seguridad
+  const trazoSeguridad: SVGPathElement = document.getElementById('trazoSeguridad') as HTMLElement & SVGPathElement;
+  const circulosSeguridad: SVGElement = document.getElementById('circulosSeguridad') as HTMLElement & SVGElement;
+
   // Calcular lugar de cada punto por lugar y pintarlos
   for (let i = 0; i < puntos.length; i++) {
     // Dibujar el primer punto
@@ -53,11 +61,15 @@ onMounted(async () => {
       const circuloHabitabilidad = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
       const circuloAmbiente = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
       const circuloInfraestructura = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      const circuloMovilidad = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      const circuloSeguridad = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
       // línea
       lineaHabitabilidad += `L 0 ${alturaContenedor - puntos[0].habitabilidad * alturaContenedor} `;
       lineaAmbiente += `L 0 ${alturaContenedor - puntos[0].ambiente * alturaContenedor} `;
       lineaInfraestructura += `L 0 ${alturaContenedor - puntos[0].infraestructura * alturaContenedor} `;
+      lineaMovilidad += `L 0 ${alturaContenedor - puntos[0].movilidad * alturaContenedor} `;
+      lineaSeguridad += `L 0 ${alturaContenedor - puntos[0].seguridad * alturaContenedor} `;
 
       // puntos Habitabilidad
       circuloHabitabilidad.setAttribute('class', 'puntoIndicador');
@@ -77,6 +89,18 @@ onMounted(async () => {
       circuloInfraestructura.setAttribute('cy', `${alturaContenedor - puntos[0].infraestructura * alturaContenedor}`);
       circulosInfraestructura.append(circuloInfraestructura);
 
+      // Puntos Movilidad
+      circuloMovilidad.setAttribute('class', 'puntoIndicador');
+      circuloMovilidad.setAttribute('cx', '0');
+      circuloMovilidad.setAttribute('cy', `${alturaContenedor - puntos[0].movilidad * alturaContenedor}`);
+      circulosMovilidad.append(circuloMovilidad);
+
+      // Puntos Seguridad
+      circuloSeguridad.setAttribute('class', 'puntoIndicador');
+      circuloSeguridad.setAttribute('cx', '0');
+      circuloSeguridad.setAttribute('cy', `${alturaContenedor - puntos[0].seguridad * alturaContenedor}`);
+      circulosSeguridad.append(circuloSeguridad);
+
       // Dibujar el resto de puntos
     } else {
       const puntoA = puntos[i - 1];
@@ -85,6 +109,8 @@ onMounted(async () => {
       const circuloHabitabilidad = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
       const circuloAmbiente = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
       const circuloInfraestructura = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      const circuloMovilidad = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      const circuloSeguridad = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
       distanciaParcial = distanciaEntreCoordenadas(puntoA.lat, puntoA.lon, puntoB.lat, puntoB.lon);
       // ir calculando la distancia total sumando las parciales
@@ -95,11 +121,15 @@ onMounted(async () => {
       const yInfraestructura = alturaContenedor - puntoB.infraestructura * alturaContenedor;
       const yHabitabilidad = alturaContenedor - puntoB.habitabilidad * alturaContenedor;
       const yAmbiente = alturaContenedor - puntoB.ambiente * alturaContenedor;
+      const yMovilidad = alturaContenedor - puntoB.movilidad * alturaContenedor;
+      const ySeguridad = alturaContenedor - puntoB.seguridad * alturaContenedor;
 
+      // Puntos Habitabilidad
       circuloHabitabilidad.setAttribute('class', 'puntoIndicador');
       circuloHabitabilidad.setAttribute('cx', `${x}`);
       circuloHabitabilidad.setAttribute('cy', `${yHabitabilidad}`);
 
+      // Puntos Ambiente
       circuloAmbiente.setAttribute('class', 'puntoIndicador');
       circuloAmbiente.setAttribute('cx', `${x}`);
       circuloAmbiente.setAttribute('cy', `${yAmbiente}`);
@@ -109,26 +139,44 @@ onMounted(async () => {
       circuloInfraestructura.setAttribute('cx', `${x}`);
       circuloInfraestructura.setAttribute('cy', `${yInfraestructura}`);
 
+      // Puntos Movilidad
+      circuloMovilidad.setAttribute('class', 'puntoIndicador');
+      circuloMovilidad.setAttribute('cx', `${x}`);
+      circuloMovilidad.setAttribute('cy', `${yMovilidad}`);
+
+      // Puntos Seguridad
+      circuloSeguridad.setAttribute('class', 'puntoIndicador');
+      circuloSeguridad.setAttribute('cx', `${x}`);
+      circuloSeguridad.setAttribute('cy', `${ySeguridad}`);
+
       if (i < puntos.length - 1) {
         lineaHabitabilidad += `L ${x} ${yHabitabilidad} `;
         lineaAmbiente += `L ${x} ${yAmbiente} `;
         lineaInfraestructura += `L ${x} ${yInfraestructura} `;
+        lineaMovilidad += `L ${x} ${yMovilidad} `;
+        lineaSeguridad += `L ${x} ${ySeguridad} `;
       } else {
         // cerrar el path si es el último punto
         lineaHabitabilidad += `L ${x} ${yHabitabilidad} L ${x} ${alturaContenedor} Z`;
         lineaAmbiente += `L ${x} ${yAmbiente} L ${x} ${alturaContenedor} Z`;
         lineaInfraestructura += `L ${x} ${yInfraestructura} L ${x} ${alturaContenedor} Z`;
+        lineaMovilidad += `L ${x} ${yMovilidad} L ${x} ${alturaContenedor} Z`;
+        lineaSeguridad += `L ${x} ${ySeguridad} L ${x} ${alturaContenedor} Z`;
       }
 
       // Agregar los círculos a los grupos
       circulosHabitabilidad.append(circuloHabitabilidad);
       circulosAmbiente.append(circuloAmbiente);
       circulosInfraestructura.append(circuloInfraestructura);
+      circulosMovilidad.append(circuloMovilidad);
+      circulosSeguridad.append(circuloSeguridad);
     }
 
     trazoHabitabilidad.setAttribute('d', lineaHabitabilidad);
     trazoAmbiente.setAttribute('d', lineaAmbiente);
     trazoInfraestructura.setAttribute('d', lineaInfraestructura);
+    trazoMovilidad.setAttribute('d', lineaMovilidad);
+    trazoSeguridad.setAttribute('d', lineaSeguridad);
   }
 });
 
@@ -158,11 +206,19 @@ function convertirEscala(
 
     <path id="trazoInfraestructura" class="trazo" />
     <g id="circulosInfraestructura"></g>
+
+    <path id="trazoMovilidad" class="trazo" />
+    <g id="circulosMovilidad"></g>
+
+    <path id="trazoSeguridad" class="trazo" />
+    <g id="circulosSeguridad"></g>
   </svg>
   <div id="etiquetas">
     <p class="etiqueta" id="etiqHabitabilidad">Habitabilidad</p>
     <p class="etiqueta" id="etiqAmbiente">Ambiente</p>
     <p class="etiqueta" id="etiqInfraestructura">Infraestructura</p>
+    <p class="etiqueta" id="etiqMovilidad">Movilidad</p>
+    <p class="etiqueta" id="etiqSeguridad">Seguridad</p>
   </div>
 </template>
 
@@ -175,7 +231,7 @@ function convertirEscala(
 }
 
 #trazoHabitabilidad {
-  stroke: rgb(65, 134, 56);
+  stroke: var(--colorHabitabilidad);
 }
 
 #trazoAmbiente {
@@ -183,7 +239,15 @@ function convertirEscala(
 }
 
 #trazoInfraestructura {
-  stroke: rgb(5, 19, 208);
+  stroke: var(--colorInfraestructura);
+}
+
+#trazoMovilidad {
+  stroke: var(--colorMovilidad);
+}
+
+#trazoSeguridad {
+  stroke: var(--colorSeguridad);
 }
 
 .etiqueta {
@@ -193,7 +257,7 @@ function convertirEscala(
 }
 
 #etiqHabitabilidad {
-  border-color: rgb(65, 134, 56);
+  border-color: var(--colorHabitabilidad);
 }
 
 #etiqAmbiente {
@@ -201,7 +265,15 @@ function convertirEscala(
 }
 
 #etiqInfraestructura {
-  border-color: rgb(5, 19, 208);
+  border-color: var(--colorInfraestructura);
+}
+
+#etiqMovilidad {
+  border-color: var(--colorMovilidad);
+}
+
+#etiqSeguridad {
+  border-color: var(--colorSeguridad);
 }
 
 .trazo {
