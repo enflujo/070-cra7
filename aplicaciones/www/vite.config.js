4875cdf1c60d969d9230ff893be42deaf38a4d91
anattolia <anattolia@gmail.com>
2024-11-27 14:29:15 -0500

    💩 3

--- aplicaciones/www/vite.config.js
+++ aplicaciones/www/vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  rollupOptions: {
-    // Add _all_ external dependencies here
-    external: ['blotter'],
-    output: {
-      globals: {
-        blotter: 'blotter',
-      },
-    },
-  },
+  plugins: [vue(), externalizeDeps()],
   clearScreen: false,
   build: {
     outDir: 'publico',
