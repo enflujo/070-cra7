dec34b8817c91489b3d7e5509b3d74a2e52e126c
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2025-03-31 12:32:28 -0500

    ♻️ Pasar datos de podcasts a cerebro

--- aplicaciones/www/src/utilidades/cerebro.ts
+++ aplicaciones/www/src/utilidades/cerebro.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import type { Cerebro } from '../tipos';
+import type { Cerebro, ElementoPaisaje } from '../tipos';
 import type { Perfil, Texto } from '@/tipos/compartidos';
 
 // Guargar qué lugar está elegido para que quede disponible para todos los componentes
@@ -24,6 +24,36 @@ export const usarCerebro = defineStore('cerebro', {
     },
   },
 });
+
+export const podcasts: ElementoPaisaje[] = [
+  {
+    id: '1',
+    ruta: 'https://open.spotify.com/embed/episode/4LjdcPIIOWgX6hw58sTZ4h',
+    nombre: 'Una ciudad habitable: ¿es chévere vivir en Bogotá?',
+    descripcion:
+      '¿Qué significa que una ciudad sea habitable? ¿Qué factores inciden en que consideremos que una ciudad es más o menos agradable para vivir? ¿Pueden los trancones y la contaminación de una ciudad afectar la salud física y mental de quienes vivimos allí? <br> <br> En este episodio de Veinticuatro Siete, la profesora y médica Olga Lucía Sarmiento nos explicará el concepto de habitabilidad urbana, y usará como ejemplo la carrera Séptima, una de las avenidas más importantes y representativas de Bogotá.',
+  },
+  {
+    id: '2',
+    ruta: 'https://open.spotify.com/embed/episode/6SOndW9Jo3nPKpzBp9IEul',
+    nombre: 'Una ciudad para moverse, una ciudad para quedarse',
+    descripcion:
+      '¿Cómo construir soluciones de movilidad para que las personas no solo piensen en moverse, en llegar de un punto a otro en una ciudad, sino que se quieran quedar a vivir allí? ¿Se puede pensar en soluciones para transportarse mejor pero también para vivir mejor? <br> <br> En este segundo episodio de Veinticuatro Siete, el profesor e ingeniero Carlos Moncada nos explica cómo pensar una movilidad sostenible para hacer ciudades más habitables.',
+  },
+  {
+    id: '3',
+    ruta: 'https://open.spotify.com/embed/episode/4KLNWodM68BNvhxlmKY7fu',
+    nombre: '¿Qué nos dicen las aves sobre la calidad del aire y el ruido en Bogotá?',
+    descripcion:
+      'En este episodio quisimos hacer algo diferente: entender los efectos que tiene vivir en una ciudad como Bogotá, no para los humanos sino para otros seres, como las aves, que habitan con nosotros desde otra altura. <br> <br> ¿Qué nos dicen las palomas, torcazas, mirlas, colibríes, reinitas y demás aves sobre el ruido y la contaminación en la capital? <br> <br> En este episodio de Veinticuatro Siete, la médica veterinaria Arlen Patricia Gómez y el profesor Ricardo Morales nos explican cómo estos factores medioambientales pueden hacer que Bogotá sea más o menos habitable para los humanos y otros seres con los que convivimos.',
+  },
+  {
+    id: '4',
+    ruta: 'https://open.spotify.com/embed/episode/0Wy6oN44v3MiBdHlwaWgnV?si=6786b72e435b4389',
+    nombre: 'Movilidad y proximidad en la carrera Séptima: 25 años de promesas incumplidas',
+    descripcion: `<p>Durante más de 25 años, la Carrera Séptima ha sido escenario de debates y planes que no han llegado a ejecución. ¿Cómo esto afecta la calidad de vida? ¿Qué rol juegan el poder, los intereses privados y la desigualdad? ¿Qué se necesita para que la Séptima sea diseñada para la gente?</p> <p>En el último episodio de <span class="resaltar">Veinticuatro/Siete</span>, el profesor e ingeniero Luis Ángel Guzmán analiza por qué la movilidad en la Séptima sigue atrapada en el mismo trancón de hace décadas.</p>`,
+  },
+];
 
 export const perfiles: Perfil[] = [
   {
